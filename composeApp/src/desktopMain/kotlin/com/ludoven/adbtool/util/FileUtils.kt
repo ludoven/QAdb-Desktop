220ef762b06f74946117c277957c622920a2e800
ludoven <xfxjl@qq.com>
2026-05-11 14:10:25 +0800

    fix(windows): use swing file chooser for adb path selection

--- composeApp/src/desktopMain/kotlin/com/ludoven/adbtool/util/FileUtils.kt
+++ composeApp/src/desktopMain/kotlin/com/ludoven/adbtool/util/FileUtils.kt
@@ -1,27 +1,88 @@
 package com.ludoven.adbtool.util
 
-
 import io.github.vinceglb.filekit.FileKit
 import io.github.vinceglb.filekit.absolutePath
 import io.github.vinceglb.filekit.dialogs.FileKitType
 import io.github.vinceglb.filekit.dialogs.openDirectoryPicker
 import io.github.vinceglb.filekit.dialogs.openFilePicker
+import javax.swing.JFileChooser
+import javax.swing.SwingUtilities
+import javax.swing.filechooser.FileNameExtensionFilter
 
 object FileUtils {
+    private val isWindows: Boolean
+        get() = System.getProperty("os.name").lowercase().contains("windows")
+
     suspend fun selectApkFile(): String? {
+        if (isWindows) {
+            return runOnEdt {
+                val chooser = JFileChooser().apply {
+                    fileSelectionMode = JFileChooser.FILES_ONLY
+                    isMultiSelectionEnabled = false
+                    fileFilter = FileNameExtensionFilter("APK Files (*.apk)", "apk")
+                }
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    chooser.selectedFile?.absolutePath
+                } else {
+                    null
+                }
+            }
+        }
+
         val file = FileKit.openFilePicker(type = FileKitType.File(listOf("apk")))
         return file?.absolutePath()
     }
 
 
     suspend fun selectFolder(): String? {
+        if (isWindows) {
+            return runOnEdt {
+                val chooser = JFileChooser().apply {
+                    fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
+                    isMultiSelectionEnabled = false
+                    isAcceptAllFileFilterUsed = false
+                }
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    chooser.selectedFile?.absolutePath
+                } else {
+                    null
+                }
+            }
+        }
+
         val openDirectoryPicker = FileKit.openDirectoryPicker()
         return openDirectoryPicker?.absolutePath()
     }
 
 
     suspend fun selectFile(): String? {
+        if (isWindows) {
+            return runOnEdt {
+                val chooser = JFileChooser().apply {
+                    fileSelectionMode = JFileChooser.FILES_ONLY
+                    isMultiSelectionEnabled = false
+                }
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    chooser.selectedFile?.absolutePath
+                } else {
+                    null
+                }
+            }
+        }
+
         val openFilePicker = FileKit.openFilePicker()
         return openFilePicker?.absolutePath()
+    }
+
+    private fun <T> runOnEdt(action: () -> T): T {
+        if (SwingUtilities.isEventDispatchThread()) {
+            return action()
+        }
+
+        var result: Result<T>? = null
+        SwingUtilities.invokeAndWait {
+            result = runCatching(action)
+        }
+        return result!!.getOrThrow()
     }
 }
